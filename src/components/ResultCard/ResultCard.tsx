94847c9474fb7dc49f70e64f7293dcf5b1051c34
Joan Cadena <inmortal_20@live.com>
2024-07-08 15:21:46 -0500

    Adding free shipping icon

--- src/components/ResultCard/ResultCard.tsx
+++ src/components/ResultCard/ResultCard.tsx
@@ -4,6 +4,7 @@ import { Input } from "reactstrap";
 import styles from "./ResultCard.module.scss";
 import Image from "next/image";
 import { Item } from "@/interfaces/items";
+import FreeShippingIcon from "@/icons/free_shipping_icon.png";
 
 export const ResultCard = ({
   itemData,
@@ -18,9 +19,12 @@ export const ResultCard = ({
         <Image alt="image" src={itemData.picture} height={96} width={96} />
       </section>
       <section className={styles.infoContainer}>
-        <h3 className={styles.price}>
-          $ {Intl.NumberFormat("es-CO").format(itemData.price.amount)}
-        </h3>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <h3 className={styles.price}>
+            $ {Intl.NumberFormat("es-CO").format(itemData.price.amount)}
+          </h3>
+          <Image alt="image" src={FreeShippingIcon} height={16} width={16} />
+        </div>
         <p className={styles.author}>{itemData.seller}</p>
         <p className={styles.title}>{itemData.title}</p>
       </section>
